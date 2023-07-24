902820109f8b4ae21e264b591f94d5a61ac8b9e8
Shreyas P <121459311+Shreyas-sh12@users.noreply.github.com>
2023-07-24 17:11:12 +0530

    Create footer.js

---
+++ exam/src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* Your footer content */}
+      Footer content here
+    </footer>
+  );
+};
+
+export default Footer;
